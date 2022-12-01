5a62e9ab47251c087a55e50a2389e757d54648db
Anton <chuvak209@gmail.com>
2022-12-01 15:46:56 +0300

    new model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Flower(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False)
+    price = models.IntegerField(null=False, blank=False)
+    quantity = models.IntegerField(null=False, blank=False)
+
+    def __str__(self):
+        return self.name
